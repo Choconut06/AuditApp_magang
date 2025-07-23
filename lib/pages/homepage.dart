134935cca8588a03d263a6ed3b01d6a4cf116488
synocra <mukhlisnurhidayat09@gmail.com>
2025-07-23 15:18:25 +0700

    tambah sidebar

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,55 +1,104 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: appBar(),
+      drawer: _buildDrawer(),
+      body: const Center(),
     );
   }
-  
 
-  //menggunakan prefersize karena appbar custom
+  // Drawer (Sidebar)
+  Drawer _buildDrawer() {
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+            decoration: const BoxDecoration(color: Colors.blue),
+            child: const Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                'Menu',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.abc),
+            title: const Text('Menu1'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.abc),
+            title: const Text('Menu2'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.abc),
+            title: const Text('Menu3'),
+            onTap: () {},
+          ),
+          const Spacer(),
+
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Logout'),
+            onTap: () {
+              // Aksi logout
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
   PreferredSizeWidget appBar() {
     return PreferredSize(
       preferredSize: const Size.fromHeight(120),
       child: AppBar(
-        automaticallyImplyLeading: false, 
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.blue[400],
         flexibleSpace: SafeArea(
           child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black)
-            ),
+            decoration: BoxDecoration(border: Border.all(color: Colors.black)),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Padding(
-                  padding:
-                      const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 0),
+                  padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      // Leading icon
-                      Container(
-                        alignment: Alignment.center,
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: Colors.blue[400],
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: SvgPicture.asset(
-                          'assets/icons/three-lines.svg',
-                          color: Colors.white,
-                          height: 40,
-                          width: 40,
+                      GestureDetector(
+                        onTap: () {
+                          _scaffoldKey.currentState?.openDrawer();
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.blue[400],
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: const Icon(
+                            Icons.menu,
+                            color: Colors.white,
+                            size: 40,
+                          ),
                         ),
                       ),
- 
-                      // Action icon
                       Container(
                         alignment: Alignment.center,
                         padding: const EdgeInsets.all(8),
@@ -57,11 +106,10 @@ class HomePage extends StatelessWidget {
                           color: Colors.blue[400],
                           borderRadius: BorderRadius.circular(10),
                         ),
-                        child: SvgPicture.asset(
-                          'assets/icons/profile.svg',
+                        child: const Icon(
+                          Icons.person,
                           color: Colors.white,
-                          height: 40,
-                          width: 40,
+                          size: 40,
                         ),
                       ),
                     ],
