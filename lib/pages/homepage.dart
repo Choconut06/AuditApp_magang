89f117290165c285c7267014757c3be8911d47da
Choconut06 <dartwc06>
2025-07-23 16:55:23 +0700

    Fixing Merge Problem

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -2,10 +2,22 @@ import 'package:audit_app_magang/model/mainmenu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class HomePage extends StatelessWidget {
-  HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   List<MainMenuModel> mainMenu = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getMainMenu();
+  }
 
   void _getMainMenu() {
     mainMenu = MainMenuModel.getMainMenu();
@@ -13,13 +25,63 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    _getMainMenu();
     return Scaffold(
+      key: _scaffoldKey,
       appBar: appBar(),
-
+      drawer: _buildDrawer(),
       body: Column(
         children: [
-          mainMenuBar()
+          mainMenuBar(),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              color: Colors.grey[100],
+              child: const Center(child: Text("On Progress")),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  // Drawer (Sidebar)
+  Drawer _buildDrawer() {
+    return Drawer(
+      child: Column(
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(color: Colors.blue),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                'Menu',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.abc),
+            title: const Text('Menu1'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.abc),
+            title: const Text('Menu2'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.abc),
+            title: const Text('Menu3'),
+            onTap: () {},
+          ),
+          const Spacer(),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Logout'),
+            onTap: () {
+              // Aksi logout
+            },
+          ),
         ],
       ),
     );
@@ -27,106 +89,99 @@ class HomePage extends StatelessWidget {
 
   SizedBox mainMenuBar() {
     return SizedBox(
-          height: 300,
-          width: double.infinity,
-          child: Stack(
-            alignment: Alignment.topCenter,
-            children: [
-              Container(
-                height: 127,
-                decoration: BoxDecoration(
-                  color: Colors.blue[400],
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
-                      spreadRadius: 2,
-                      blurRadius: 5,
-                      offset: Offset(0, 3)
-                    )
-                  ]
+      height: 300,
+      width: double.infinity,
+      child: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Container(
+            height: 127,
+            decoration: BoxDecoration(
+              color: Colors.blue[400],
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                  offset: const Offset(0, 3),
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            top: 10,
+            child: Container(
+              height: 219,
+              width: 384,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey.shade400),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: GridView.count(
+                  crossAxisCount: 4,
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: List.generate(mainMenu.length, (index) {
+                    final item = mainMenu[index];
+                    return GestureDetector(
+                      onTap: () {
+                        print('Pindah ke halaman ${item.name}');
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              color: item.boxColor,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: SvgPicture.asset(
+                                item.iconPath,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(item.name),
+                        ],
+                      ),
+                    );
+                  }),
                 ),
               ),
-
-              Positioned(
-                top: 10,  
-                child: Container(
-                  height: 219,
-                  width: 384,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(
-                      color: Colors.grey.shade400
-                    ),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(10),
-                    child: GridView.count(
-                      crossAxisCount: 4,
-                      physics: const NeverScrollableScrollPhysics(),
-                      children: List.generate(mainMenu.length, (index){
-                        final item = mainMenu[index];
-                        return GestureDetector(
-                          onTap: (){
-                            print('Pindah ke halaman ${item.name}');
-                          },
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Container(
-                                height: 50,
-                                width: 50,
-                                decoration: BoxDecoration(
-                                  color: item.boxColor,
-                                  borderRadius: BorderRadius.circular(8)
-                                ),
-                                child: Padding(
-                                  padding: EdgeInsets.all(10),
-                                  child: SvgPicture.asset(
-                                    item.iconPath
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 8,
-                              ),
-                              Text(item.name),
-                            ],
-                          ),
-                        );
-                      }
-                      ),
-                    ),
-                  )
-                ),
-              )
-            ],
-          ),
-        );
+            ),
+          )
+        ],
+      ),
+    );
   }
 
-
-  //menggunakan prefersize karena appbar custom
   PreferredSizeWidget appBar() {
     return PreferredSize(
       preferredSize: const Size.fromHeight(120),
       child: AppBar(
-        automaticallyImplyLeading: false, 
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.blue[400],
         flexibleSpace: SafeArea(
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // Leading icon
-                      Container(
-                        alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Menu Button
+                    GestureDetector(
+                      onTap: () {
+                        _scaffoldKey.currentState?.openDrawer();
+                      },
+                      child: Container(
                         padding: const EdgeInsets.all(8),
                         decoration: BoxDecoration(
                           color: Colors.blue[400],
@@ -139,27 +194,26 @@ class HomePage extends StatelessWidget {
                           width: 40,
                         ),
                       ),
- 
-                      // Action icon
-                      Container(
-                        alignment: Alignment.center,
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: Colors.blue[400],
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: SvgPicture.asset(
-                          'assets/icons/profile.svg',
-                          color: Colors.white,
-                          height: 40,
-                          width: 40,
-                        ),
+                    ),
+
+                    // Profile icon
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                    ],
-                  ),
+                      child: SvgPicture.asset(
+                        'assets/icons/profile.svg',
+                        color: Colors.white,
+                        height: 40,
+                        width: 40,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
